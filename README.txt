78f6964c0d8bd17cc0f32d7ed84de0f3d3458675
Caelan <33264757+caelan-smyth@users.noreply.github.com>
2019-04-10 12:18:02 +0100

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,30 @@
+MyShell
+
+A simple shell built in Python.
+
+Commands:
+
+cd <directory>
+- Changes the current working directory to <directory>.
+- If no <directory> argument is passed, reports the current working directory.
+
+dir <directory>
+- Lists the contents of the directory <directory>.
+
+clr
+- Clears the screen. Does not take any arguments.
+
+environ
+- Lists all of the environment strings.
+
+echo <comment>
+- Displays <comment> on the screen with all excess whitespace stripped, followed by a new line.
+
+help
+- Displays the user manual using the more filter.
+
+pause
+- Pauses shell operation until the Enter key is pressed.
+
+quit
+- Exits the shell.
--- myshell.py
+++ myshell.py
@@ -2,32 +2,73 @@ from cmd import Cmd
 import os, sys
 
 class myshell(Cmd):
-    def cd(s):
-        try:
-            if s:
-                os.chdir(s)
-            else:
-                os.getcwd()
-        except:
-            print(s + ": No such file or directory")
+    prompt = "~" + os.getcwd() + ":" + "~" + "$ "
+    starter = "Welcome to the shell! Type ? to list commands."
 
-    def dir(s):
+    def do_quit(self, key):
+        print("Exiting...")
+        return True
+
+    def help_quit(self):
+        print("Exits the shell.")
+
+    def do_cd(s):
+        if s:
+            try:
+                os.chdir(s)
+                path = os.getcwd()
+                os.environ["PWD"] = os.getcwd()
+                shell.prompt = "~" + path + ":" + "~" + "$ "
+            except:
+                print(s + ": No such file or directory. Do 'help cd' for help.")
+        else:
+            os.getcwd()
+
+    def help_cd():
+        print("Changes the current working directory.\nUsage: cd <directory>")
+
+    def do_dir(s):
         os.listdir(s)
 
-    def clr():
+    def help_dir():
+        print("Lists the contents of the directory.\nUsage: dir <directory>")
+
+    def do_clr():
         os.system('clear')
 
-    def environ():
+    def help_clr():
+        print("Clears the screen.")
+
+    def do_environ():
         os.environ()
 
-    def echo(s):
+    def help_environ():
+        print("Prints the environment strings.")
+
+    def do_echo(s):
         print(' '.join(s.split()) + "\n")
 
-    def pause():
-        os.system('pause')
+    def help_echo():
+        print("Prints a comment on the screen with excess whitespace stripped and a newline at the end.\nUsage: echo <comment>")
 
-def main():
+    def do_pause():
+        input("Press Enter to continue.")
+
+    def help_pause():
+        print("Pauses shell operation until the Enter key is pressed.")
+
+    def do_help():
     
 
 if __name__ == '__main__':
-    main()
+    shell = myShell()
+    path = os.getcwd()
+    print(sys.argv)
+
+    if len(sys.argv) > 1:
+        with open(sys.argv[1]) as file:
+            for line in file.readlines():
+                shell.onecmd(line)
+
+    else:
+        shell.cmdloop()
